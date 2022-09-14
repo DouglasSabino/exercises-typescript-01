d5ca5ff3341c93c674f7b7c35f665108726be449
Douglas Sabino <douglasbsabino@hotmail.com>
2022-09-14 04:47:50 -0300

    exercicio 4

---
+++ exercises/Months.js
@@ -0,0 +1,19 @@
+"use strict";
+// Exercício 2: Crie uma enum que represente os meses do ano.
+exports.__esModule = true;
+var Months;
+(function (Months) {
+    Months["JANEIRO"] = "Janeiro";
+    Months["FEVEREIRO"] = "Fevereiro";
+    Months["MARCO"] = "Mar\u00E7o";
+    Months["ABRIL"] = "Abril";
+    Months["MAIO"] = "Maio";
+    Months["JUNHO"] = "Junho";
+    Months["JULHO"] = "Julho";
+    Months["AGOSTO"] = "Agosto";
+    Months["SETEMBRO"] = "Setembro";
+    Months["OUTUBRO"] = "Outubro";
+    Months["NOVEMBRO"] = "Novembro";
+    Months["DEZEMBRO"] = "Dezembro";
+})(Months || (Months = {}));
+exports["default"] = Months;
---
+++ exercises/Seasons.js
@@ -0,0 +1,11 @@
+"use strict";
+// Exercício 3: Crie uma enum que represente as estações climáticas do ano.
+exports.__esModule = true;
+var Seasons;
+(function (Seasons) {
+    Seasons["OUTONO"] = "Outono";
+    Seasons["INVERNO"] = "Inverno";
+    Seasons["PRIMAVERA"] = "Primavera";
+    Seasons["VERAO"] = "Ver\u00E3o";
+})(Seasons || (Seasons = {}));
+exports["default"] = Seasons;
